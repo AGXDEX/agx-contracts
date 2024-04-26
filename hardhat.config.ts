d6e35ca6eec5f8fe42a3e7f6d14a9dda1f5802fd
ChenJing <hao286242341@gmail.com>
2024-04-26 16:51:04 +0800

    change hardhat config, delete position router and position router reader

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,25 +1,63 @@
-import { HardhatUserConfig } from 'hardhat/config';
-import '@nomicfoundation/hardhat-toolbox';
-import '@matterlabs/hardhat-zksync-deploy';
-import '@matterlabs/hardhat-zksync-solc';
-import '@matterlabs/hardhat-zksync-verify';
-import '@matterlabs/hardhat-zksync-upgradable';
-import "@matterlabs/hardhat-zksync-node";
-import 'solidity-coverage';
-import '@openzeppelin/hardhat-upgrades';
-import * as dotenv from 'dotenv';
-import { NetworksUserConfig, NetworkUserConfig } from 'hardhat/src/types/config';
+import { HardhatUserConfig } from "hardhat/config";
 
+import "@matterlabs/hardhat-zksync";
+import * as dotenv from "dotenv";
 dotenv.config();
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-const config: HardhatUserConfig = {
+const PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY || "";
 
-  typechain: {
-    outDir: 'typechain',
-    target: 'ethers-v6',
+const config: HardhatUserConfig = {
+  defaultNetwork: "novaSepolia",
+  networks: {
+    zkSyncSepoliaTestnet: {
+      url: "https://sepolia.era.zksync.dev",
+      ethNetwork: "sepolia",
+      zksync: true,
+      verifyURL: "https://explorer.sepolia.era.zksync.dev/contract_verification",
+    },
+    zkSyncMainnet: {
+      url: "https://mainnet.era.zksync.io",
+      ethNetwork: "mainnet",
+      zksync: true,
+      verifyURL: "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
+    },
+    zkSyncGoerliTestnet: { // deprecated network
+      url: "https://testnet.era.zksync.dev",
+      ethNetwork: "goerli",
+      zksync: true,
+      verifyURL: "https://zksync2-testnet-explorer.zksync.dev/contract_verification",
+    },
+    dockerizedNode: {
+      url: "http://localhost:3050",
+      ethNetwork: "http://localhost:8545",
+      zksync: true,
+    },
+    inMemoryNode: {
+      url: "http://127.0.0.1:8011",
+      ethNetwork: "localhost", // in-memory node doesn't support eth node; removing this line will cause an error
+      zksync: true,
+    },
+    novaSepolia:{
+      url: "https://rpc.zklink.io",
+      ethNetwork: "mainnet",
+      zksync: true,
+      accounts:[PRIVATE_KEY]
+    },
+    hardhat: {
+      zksync: true,
+    },
+  },
+  zksolc: {
+    version: "latest",
+    settings: {
+      // find all available options in the official documentation
+      // https://era.zksync.io/docs/tools/hardhat/hardhat-zksync-solc.html#configuration
+        libraries: {
+              "contracts/core/PositionUtils.sol": {
+                "PositionUtils": "0xb2C8Af5100AFcdF53AE5e8a878EC14c1F854CEE8"
+              }
+            }
+    },
   },
   solidity: {
     compilers: [
@@ -50,36 +88,9 @@ const config: HardhatUserConfig = {
           },
         },
       }
-    ],
-  },
-  defaultNetwork: 'zklinkGoerli',
-  networks: {
-    hardhat: {
-      zksync: false,
-    },
 
-    zklinkGoerli: {
-      url: 'https://goerli.rpc.zklink.io',
-      ethNetwork: 'goerli',
-      verifyURL: 'https://goerli.explorer.zklink.io/contract_verification',
-      zksync: true,
-    },
-    zklinkNova: {
-      url: 'https://rpc.zklink.io',
-      ethNetwork: 'mainnet',
-      verifyURL: 'https://explorer.zklink.io/contract_verification',
-      zksync: true,
-    },
-  },
-  zksolc: {
-    version: '1.3.22',
-    settings: {},
-  },
-  mocha: {
-    timeout: 600000,
+    ]
   },
 };
-
-
 
 export default config;
