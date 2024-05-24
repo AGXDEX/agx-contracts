60c3e16d20344c3dbaceb940c2c76112a23212be
ChenJing <hao286242341@gmail.com>
2024-05-24 14:28:22 +0800

    update script

--- deploy/staker/createIncentive.ts
+++ deploy/staker/createIncentive.ts
@@ -7,6 +7,7 @@ import price from "../../pricefeed.json";
 import ContractAddresses from "../../DeploymentOutput.json";
 import {NFT_REWARD_AMOUNT} from "../../config";
 import factoryArtifact from "../../externalABI/UniswapV3Factory.json";
+import fs from "fs";
 
 async function main() {
 
@@ -49,9 +50,11 @@ async function main() {
     };
 
     console.log(args);
+    const deploymentStateJSON = JSON.stringify(args, null, 2);
+    fs.writeFileSync("./Incentive.json", deploymentStateJSON);
 
     const createIncentive = await v3Staker.createIncentive(
-       args, config.NFT_REWARD_AMOUNT);
+        args, config.NFT_REWARD_AMOUNT);
     await createIncentive.wait();
 
     console.log(createIncentive.hash);
